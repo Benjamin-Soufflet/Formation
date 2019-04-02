8cf71a55ee7f516b36bd839306bd3c6ceff11ca1
Ben <benjamin.soufflet12@gmail.com>
2019-04-02 21:56:22 +0200

    JQuery + changement dossier Color_Projet.

---
+++ Components/Javascript/Jquery/Effects/effects.js
@@ -0,0 +1,14 @@
+$("button").on("click", function(){
+ $('div').slideToggle(1000, function(){
+ 	$(this).remove();//remove the divs AFTER the slideToggle is finished
+ });
+});
+
+// //Sans Jquery
+// divs = document.querySelectorAll("div");
+// document.querySelector("button").addEventListener("click",function(){
+// 	for (var i = 0; i < divs.length; i++) {
+// 		divs[i].classList.toggle("notHidden");
+// 		divs[i].classList.toggle("hidden");
+// 	}
+// });
